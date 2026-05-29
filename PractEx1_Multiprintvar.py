7e9d921c5f3ef3dacf3982c234d24420d076efc0
Vinay Kumar Hanumanarasaiah <vinayhvg@gmail.com>
2026-05-29 22:27:05 +0530

    PractEx1_Multiprintvar

---
+++ PractEx1_Multiprintvar.py
@@ -0,0 +1,4 @@
+a = input("Enter the value of a: ")
+n = int(input("Enter the valur of n: "))
+print(a*n)
+# here the value of a is string and n is integer so when we multiply a with n it will repeat the string n times. For example if a is "hello" and n is 3 then it will print "hellohellohello".   
