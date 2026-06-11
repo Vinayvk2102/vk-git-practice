d8da0c8f9f1a2794d2768a32055299951f292899
Vinay Kumar Hanumanarasaiah <vinayhvg@gmail.com>
2026-06-11 12:55:02 +0530

    if-statementexamp1

---
+++ PractEx4_Ifstatements.py
@@ -0,0 +1,14 @@
+x = int(input("Enter a number: x = "))
+y = int(input("Enter another number: y = "))
+if x < y:
+    print("x is Smaller")
+if x > y:
+    print("x is Bigger")
+if y < x:
+    print("y is Smaller")
+if y > x:
+    print("y is Bigger")
+if x == y:
+    print("x and y are Equal")
+
+print("Finish")
